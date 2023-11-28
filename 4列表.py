d3c4b007369c021eb83a5e97c632057f72c4da2d
yulang <1768788178@qq.com>
2023-11-28 16:56:32 +0800

    feat:列表

---
+++ 4列表.py
@@ -0,0 +1,15 @@
+# 创建列表
+list1= [1,3,5]
+list2=[x for x in range(1,10)]
+list3=list('123')
+print(list1,list2,list3)
+# 修改
+list1[1]=2
+# 删除
+del list1[0]
+del lsit1
+# 添加
+list1.insert(索引，参数)
+lsit1.append(值) 在末尾添加元素
+list1.extend(可迭代对象)
+
