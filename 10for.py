02c54202e7414461c84ee3f7938cb80d69b24218
yulang <1768788178@qq.com>
2023-11-29 10:57:30 +0800

    feat:判断与循环

---
+++ 10for.py
@@ -0,0 +1,15 @@
+#  for i  in xxxx(可遍历对象)
+
+# for i in range(1,10):
+#   print(i)
+
+# for i in 'abc':
+#   print(i)
+  
+# for i  in ('1','2','3'):
+#   print(i)
+# for i  in tuple('9000'):
+#   print(i)
+
+for  i in set({'a','b','c'}):
+  print(i)
---
+++ 9while.py
@@ -0,0 +1,7 @@
+
+num =100
+while (num>90):
+   num=num-1
+   print(num)
+   continue
+   
