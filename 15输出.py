76a0400cbca4c0f75f51abcdfc2c0c24699fc5d7
yulang <1768788178@qq.com>
2023-12-20 19:39:00 +0800

    f-string

---
+++ 15输出.py
@@ -0,0 +1,17 @@
+# 1. 百分号%输出
+# a=int(input("请输入一个数字1:"))
+# b=int(input("请输入一个数字2:"))
+# a = 1
+# b = 2
+# print('%s is %s', %(1, 2))
+
+# 2.format()函数
+
+# print("{1} is {0}".format(1, 2))  // 2 is 1
+
+
+# 3f-string(一般使用)
+
+a=2 
+b=3
+print(f"{a} is { b}")  # 2 is 3
