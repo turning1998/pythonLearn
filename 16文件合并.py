e6e6572a72422e18b0a5f5a544ee476796db1cb8
yulang <1768788178@qq.com>
2023-12-21 21:13:02 +0800

    feat:文件的合并

---
+++ 16文件合并.py
@@ -0,0 +1,15 @@
+# 合并文件 =读取原始文件+追加写入新的文件
+# 打开a.txt,b.txt,合并生成c.txt
+# 1.打开原始文件
+# 2.写入模式创建并打开要合并的文件
+# 关闭文件 
+ 
+with open('a.txt') as f1, open('b.txt') as f2:
+  content1=f1.read()
+  content2=f2.read()
+
+# print(content1,content2)
+
+with open('c.txt','w') as f3:
+  f3.write(content1+content2)
+  
