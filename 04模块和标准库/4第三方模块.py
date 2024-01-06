99942a12ee87245701b94976bad1b908fdd15bc3
yulang <1768788178@qq.com>
2024-01-06 15:17:44 +0800

    save:第三方模块

--- 04模块和标准库/4第三方模块.py
+++ 04模块和标准库/4第三方模块.py
@@ -3,23 +3,35 @@
 # pip3.10 install —— 第三⽅模块名称
 # python3.10 -m pip install —— 第三⽅模块名称
 # * https://pypi.org/project/requests/
+
+# pip install requests 
+# 或者 python3  -m pip install requests
+
+# 不同程序要求的python版本不一样
 # 虚拟环境
 # 虚拟环境的⽤途：
 # • 解决多个模块依赖的问题
 # • ⼀次性安装多个指定版本的模块
 # • 避免对默认环境造成污染
 # 创建虚拟环境：
-# python -m venv myvenv
+# python -m venv myvenv 【执行后目录下多了myvenv】下面是虚拟环境python的包
+
+
 # • venv 虚拟环境模块
 # • myvenv 保存虚拟环境的⽂件夹
 # 将当前安装的包及其版本保存到⽂件中：
-# pip3.10 freeze > requirements.txt
+# pip3.10 freeze 【查看当前的文件包】
+# pip3.10 freeze > requirements.txt 【保存到⽂件中】
 # • 激活虚拟环境：
 # source myvenv/bin/activate
+
 # • 在虚拟环境中导⼊指定的包：
 # pip3.10 install -r requirements.txt
+
 # • 离开虚拟环境：
 # deactivate
+
+
 # 加速第三⽅模块安装
 # • 临时加速
 # pip install -i https://pypi.tuna.tsinghua.edu.cn/simple package_name
